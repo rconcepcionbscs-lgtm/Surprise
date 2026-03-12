c13b71ddaeb1ce8080a5978659cfd934d8b8d42f
rconcepcionbscs-lgtm <rconcepcionbscs@gmail.com>
2026-03-12 12:49:21 +0800

    Add files via upload

---
+++ ayej.js
@@ -0,0 +1,26 @@
+function showCard() {
+  const card = document.getElementById('hbdcard');
+  const song = document.getElementById('mySong');
+  
+  //show the card
+  card.style.opacity = "1";
+  card.style.pointerEvents = "auto";
+  
+  //play the music
+  song.play ();
+  document.getElementById("hbdcard").style.opacity = "1";
+  document.getElementById("hbdcard").style.pointerEvents = "auto;"
+}
+
+function closeCard() {
+  const card = document.getElementById('hbdcard');
+  const song = document.getElementById('mySong');
+  
+  // hide the card
+  card.style.opacity = "0";
+  card.style.pointerEvents = "none";
+  
+  //stop the music and reset to the beginning
+  song.pause();
+  song.currentTime = 0;
+}
