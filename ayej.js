0fef3b7b1e986a9defcdb307ec304fa755cf6a97
rconcepcionbscs-lgtm <rconcepcionbscs@gmail.com>
2026-03-25 23:50:46 +0800

    Refactor card display and song play logic
    Removed unnecessary whitespace in song.play() and adjusted style properties.

--- ayej.js
+++ ayej.js
@@ -3,11 +3,11 @@ function showCard() {
   const song = document.getElementById('mySong');
   
   //show the card
-  card.style.opacity = "1";
-  card.style.pointerEvents = "auto";
+card.style.opacity = "1";
+card.style.pointerEvents = "auto";
   
   //play the music
-  song.play ();
+  song.play();
   document.getElementById("hbdcard").style.opacity = "1";
   document.getElementById("hbdcard").style.pointerEvents = "auto;"
 }
